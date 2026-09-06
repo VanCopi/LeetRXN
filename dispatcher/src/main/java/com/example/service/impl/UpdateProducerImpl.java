e37ef2c69c0bd82645c2e5f6c2cb23f6b56f167a
Van <sidorenkov-ia@yandex.ru>
2026-09-06 12:56:24 +0300

    * Queues were added and tested in Rabbitmq web host

--- dispatcher/src/main/java/com/example/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/com/example/service/impl/UpdateProducerImpl.java
@@ -3,14 +3,22 @@ package com.example.service.impl;
 import com.example.controller.TelegramBot;
 import com.example.service.UpdateProducer;
 import org.apache.log4j.Logger;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Service
 public class UpdateProducerImpl implements UpdateProducer {
+    private final RabbitTemplate rabbitTemplate;
     private static final Logger log = Logger.getLogger(TelegramBot.class);
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
